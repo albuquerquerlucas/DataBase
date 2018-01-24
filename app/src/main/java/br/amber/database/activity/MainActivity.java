39af71ff5779c4391e982f65d8d0896881fc9208
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2018-01-24 16:08:04 -0300

    Alterado o extends da activity

--- app/src/main/java/br/amber/database/activity/MainActivity.java
+++ app/src/main/java/br/amber/database/activity/MainActivity.java
@@ -1,20 +1,23 @@
 package br.amber.database.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.amber.database.R;
 import br.amber.database.dao.DependenteDAO;
 import br.amber.database.dao.OrcamentosDAO;
+import br.amber.database.dao.StatusDAO;
 import br.amber.database.dao.TiposDocsDAO;
 import br.amber.database.dao.TitularDAO;
 import br.amber.database.util.Inserts;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private OrcamentosDAO orcDao;
     private TitularDAO tDao;
     private DependenteDAO dDao;
+    private StatusDAO sDAO;
     private TiposDocsDAO tipDao;
 
     @Override
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         this.orcDao = new OrcamentosDAO(this);
         this.tDao = new TitularDAO(this);
         this.dDao = new DependenteDAO(this);
+        this.sDAO = new StatusDAO(this);
         this.tipDao = new TiposDocsDAO(this);
     }
 
@@ -32,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        //this.orcDao.insertOrcamento(Inserts.getOrcamento());
-        //this.tDao.insertTitular(Inserts.getTitular());
-        //this.dDao.insertDependente(Inserts.getDependente());
-        //this.tipDao.insertTipoDocs(Inserts.getTiposDocumento());
+        this.orcDao.insertOrcamento(Inserts.getOrcamento());
+        this.tDao.insertTitular(Inserts.getTitular());
+        this.dDao.insertDependente(Inserts.getDependente());
+        this.sDAO.insertStatus(Inserts.getStatus());
+        this.tipDao.insertTipoDocs(Inserts.getTiposDocumento());
     }
 }
