53e3e3172331ba5ce380d276a8927b9a7c9c6225
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2018-01-25 14:34:37 -0300

    Criado testes e inicializações do banco

--- app/src/main/java/br/amber/database/activity/MainActivity.java
+++ app/src/main/java/br/amber/database/activity/MainActivity.java
@@ -3,6 +3,8 @@ package br.amber.database.activity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import br.amber.database.R;
 import br.amber.database.dao.DependenteDAO;
@@ -10,7 +12,9 @@ import br.amber.database.dao.OrcamentosDAO;
 import br.amber.database.dao.StatusDAO;
 import br.amber.database.dao.TiposDocsDAO;
 import br.amber.database.dao.TitularDAO;
+import br.amber.database.entity.TitularPojo;
 import br.amber.database.util.Inserts;
+import br.amber.database.util.Mount;
 
 public class MainActivity extends Activity {
 
@@ -36,10 +40,31 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
-        this.orcDao.insertOrcamento(Inserts.getOrcamento());
-        this.tDao.insertTitular(Inserts.getTitular());
-        this.dDao.insertDependente(Inserts.getDependente());
-        this.sDAO.insertStatus(Inserts.getStatus());
-        this.tipDao.insertTipoDocs(Inserts.getTiposDocumento());
+//        this.orcDao.insertOrcamento(Inserts.getOrcamento());
+//        this.tDao.insertTitular(Inserts.getTitular());
+//        this.tDao.inserirTitularSeNaoExistir(Inserts.getTitular());
+//        this.dDao.insertDependente(Inserts.getDependente());
+//        this.sDAO.insertStatus(Inserts.getStatus());
+//        this.tipDao.insertTipoDocs(Inserts.getTiposDocumento());
+
+//        int qtd = this.tDao.qtdDeTitulares();
+//        Toast.makeText(this, "QTD Registros = " + qtd, Toast.LENGTH_SHORT).show();
+//
+//        for(TitularPojo t : this.tDao.listarTodosOsTitulares()){
+//            Log.i("TITULAR CPF ", t.getCpf_titular());
+//        }
+//
+//        TitularPojo titular = this.tDao.getTitularPorOrcamento("332850750");
+//        Log.i("TITULAR POR ORCAMENTO ", titular.getCpf_titular());
+//
+//        TitularPojo titular2 = this.tDao.getTitularPorCpf(titular.getCpf_titular());
+//        Log.i("TITULAR POR ORCAMENTO ", titular2.getCpf_titular());
+//
+//        this.tDao.excluirTitular(titular2.getCpf_titular());
+//        Toast.makeText(this, "Titular " + titular2.getCpf_titular() + " Excluído!", Toast.LENGTH_LONG).show();
+//
+//        int qtd1 = this.tDao.qtdDeTitulares();
+//        Toast.makeText(this, "Nova QTD Registros = " + qtd1, Toast.LENGTH_SHORT).show();
+
     }
 }
